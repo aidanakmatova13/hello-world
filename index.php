1d104a568d1d1d23cdc37b67e16a0741af2a96c0
aidanakmatova13 <79746162+aidanakmatova13@users.noreply.github.com>
2021-03-02 10:07:56 +0600

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 	<div>welcome to my first site</div>
 	<div>Hello World <br>Aidana Akmatova</div>
 	<a href="https://www.youtube.com/watch?v=8WVXk0Gz66E"><button>here</button></a>
+	<img src="https://www.pommietravels.com/wp-content/uploads/2019/12/seaside-994605.jpg" alt="beach">
 	<?php echo '<p>i am aidana</p>' ?>
 
 
