21b400656db57baada21d0e4d0e1a5e4bc48278b
aidanakmatova13 <79746162+aidanakmatova13@users.noreply.github.com>
2021-03-02 10:00:55 +0600

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 	<div>welcome to my first site</div>
-	<div>woruhgbvjkyi;KBV <br>YLYK>SBrV</div>
+	<div>Hello World <br>Aidana Akmatova</div>
 	<a href="https://www.youtube.com/watch?v=8WVXk0Gz66E"><button>here</button></a>
 	<?php echo '<p>i am aidana</p>' ?>
 
