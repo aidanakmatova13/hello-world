4e3d0cf1a55ed99c1e483efdc1c45dd2d28f8336
aidanakmatova13 <79746162+aidanakmatova13@users.noreply.github.com>
2021-03-02 08:36:13 +0600

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 	<div>aidana aidnareuighalkgbuo;gb/GUOGB/SB</div>
 	<div>woruhgbvjkyi;KBV <br>YLYK>SBrV</div>
 	<a href="https://www.youtube.com/watch?v=8WVXk0Gz66E"><button>take</button></a>
+	<php  'uki' ?>
 
 
 </body>
