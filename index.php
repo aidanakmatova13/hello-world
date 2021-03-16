6d795b6b674d92e3266493a6a398a3b440323d2e
aidanakmatova13 <79746162+aidanakmatova13@users.noreply.github.com>
2021-03-16 10:39:06 +0600

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html style="background: white url('https://thewallpaper.co/wp-content/uploads/2020/09/abstract-wallpaper-hd-artworks-cool-desktop-images-art-colorful-backgrounds-cute-background-images-lovely-1697x1100-1.jpg')">
 	
 <head>
+	<meta charset="utf-8">
+  <title>Поле с паролем</title>
 	<title>aidana</title>
 <style>
 img {
@@ -16,6 +18,9 @@ img {
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
+
+
+
 .fa {
   padding: 20px;
   font-size: 30px;
@@ -36,45 +41,55 @@ img {
   color: white;
 }
 
-
+.fa-google {
+  background: #dd4b39;
+  color: white;
 </style>
 </head>
 <body>
-	<div>
-        <form action="handler.php">
-   <p><strong style="color:black " >Логин:</strong> 
+
+	<form action="handler.php">
+   <p><strong>Логин:</strong> 
     <input maxlength="25" size="40" name="login"></p>
-   <p><strong style="color: black">Пароль:</strong> 
+   <p><strong>Пароль:</strong> 
     <input type="password" maxlength="25" size="40" name="password"></p>
-    <p><input type="submit" value="Отправить"></p>
-      </div>
-	
-	
-	
-	
-	
-	
-	
-	
-	
+   <p><input type="submit" value="Войти"></p>
+  </form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	<h1 style="color:white"><center>Hey, I'm Aidana!</center></h1>
 
 
 	<p><i style="color:#E0E0E0">Web developer from Bishkek.</i></p>
 	<p><i style="color:#E0E0E0">Welcome to my Portfolio.</i> </p> 
     <p><i style="color:#E0E0E0">I create custom websites  to help busnisses do better online.</i></p>
-	<p><i style="color:#E0E0E0"> Age: 19 years</i></p>
+    <p><i style="color:#E0E0E0"> Age: 19 years</i></p>
+    
+	
 	<button type="button" onclick="alert('Hello world!')">My projects</button>
-	<img src="https://lh3.googleusercontent.com/wCvjEF_5hv5MIRrOf0eLIpq5HlbqVJf23slX_JDSy3uuDcUqg2jhATuxtKVs9K7crx4W=s85"
+	<img src="https://lh3.googleusercontent.com/wCvjEF_5hv5MIRrOf0eLIpq5HlbqVJf23slX_JDSy3uuDcUqg2jhATuxtKVs9K7crx4W=s85" 
 	style="width:200px; height:250px;">
+	
 	<p> .</p>
 	<p> .</p>
-	<p> .</p>
-	<p> .</p>
+	
 
 <h2 style="color: white">Me in social media</h2>
 <a href="https://youtube.com/channel/UCVhTYda6nxDhOIZ02TABHRg" class="fa fa-youtube"></a>
 <a href="https://instagram.com/aakmatova__?igshid=1hbwwstdylu7b" class="fa fa-instagram"></a>
+<a href="#" class="fa fa-google"></a>
 
 
 
