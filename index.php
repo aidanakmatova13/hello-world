8c57c3f52bf9cd83d94745aebcee00e617f3a9a2
aidanakmatova13 <79746162+aidanakmatova13@users.noreply.github.com>
2021-03-16 11:57:51 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,49 +1,17 @@
 <!DOCTYPE html>
-<html style="background: white url('https://images.wallpaperscraft.com/image/grass_spikelets_dry_143831_1920x1080.jpg')">
+<html style="background: white url('https://s3.eu-central-1.wasabisys.com/devonilx7/2020/05/grass_dry_plants_15.jpg')">
 	
 <head>
 	<meta charset="utf-8">
   <title>Поле с паролем</title>
-	<title>aidana</title>
-<style>
-img {
-  display: block;
-  margin-left: auto;
- 
-}
+	
 
-
-
-</style>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
 
 
 
-.fa {
-  padding: 20px;
-  font-size: 30px;
-  width: 50px;
-  text-align: center;
-  text-decoration: none;
-  margin: 5px 2px;
-}
-
-
-.fa-youtube {
-  background: #bb0000;
-  color: white;
-}
-
-.fa-instagram {
-  background: #125688;
-  color: white;
-}
-
-.fa-google {
-  background: #dd4b39;
-  color: white;
 </style>
 </head>
 <body>
@@ -55,41 +23,6 @@ img {
     <input type="password" maxlength="25" size="40" name="password"></p>
    <p><input type="submit" value="Войти"></p>
   </form>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	<h1 style="color:white"><center>Hey, I'm Aidana!</center></h1>
-
-
-	<p><i style="color:#E0E0E0">Web developer from Bishkek.</i></p>
-	<p><i style="color:#E0E0E0">Welcome to my Portfolio.</i> </p> 
-    <p><i style="color:#E0E0E0">I create custom websites  to help busnisses do better online.</i></p>
-    <p><i style="color:#E0E0E0"> Age: 19 years</i></p>
-    
-	
-	<button type="button" onclick="alert('Hello world!')">My projects</button>
-	<img src="https://w7.pngwing.com/pngs/49/229/png-transparent-barbie-girl-silhouette-drawing-hair-silhouette-logo-monochrome-black-thumbnail.png" 
-	style="width:200px; height:250px;">
-	
-	<p> .</p>
-	<p> .</p>
-	
-
-<h2 style="color: white">Me in social media</h2>
-<a href="https://youtube.com/channel/UCVhTYda6nxDhOIZ02TABHRg" class="fa fa-youtube"></a>
-<a href="https://instagram.com/aakmatova__?igshid=1hbwwstdylu7b" class="fa fa-instagram"></a>
-<a href="#" class="fa fa-google"></a>
 
 
 
