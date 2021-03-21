185f87cbb87ad9e1b1a82133f9fb5fe12cadc255
aidanakmatova13 <79746162+aidanakmatova13@users.noreply.github.com>
2021-03-22 05:39:07 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,275 @@
 <!DOCTYPE html>
-<html style="background: white url('https://images.pexels.com/photos/262367/pexels-photo-262367.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260')">
-	
-<head>
-	<meta charset="utf-8">
-  <title>Поле с паролем</title>
-	
+<html style="background: white url('https://3.bp.blogspot.com/-3-xBXaIqgvg/WKnFQwcMvlI/AAAAAAAAAog/n091eXxhFt07cEl-SfyAJEYsJWAZvdOVwCLcB/w1200-h630-p-k-no-nu/Dark%2BRoom%2BWith%2BLight.jpg')">
 
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <title>aidana</title>
 <style>
+  h3{
+    color:white; 
+  }
+  h2{
+    color:white;
+
+  }
+  h1{
+    color:white;
+
+  }
+  body{
+    font-family:  Garamond, serif;
+  }
+
+.fa {
+  padding: 20px;
+  font-size: 30px;
+  width: 50px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+}
+
+
+.fa-google {
+  background: #dd4b39;
+  color: white;
+}
+
+
+.fa-youtube {
+  background: #bb0000;
+  color: white;
+}
+
+.fa-instagram {
+  background: #125688;
+  color: white;
+}
+
+
+
+.fa-skype {
+  background: #00aff0;
+  color: white;
+}
+h2{
+color: tomato;
+}
 
 
 
 </style>
+<style type="text/css"></style>
+
+</head>
+<body>
+<h1><CENTER>HI, WELCOME TO MY PORTFOLIO!</CENTER></style></h1>
+ <div style="text-align: right;"><img src="https://media.istockphoto.com/vectors/woman-face-silhouette-girl-with-a-ponytail-portrait-female-head-side-vector-id1133423241?k=6&m=1133423241&s=170667a&w=0&h=ief1vcqlVh-qjzyJt80QRwXSiJdRsup6vfl5qD-xYaw=" width="160spx", height="200px"></div>
+
+ </div>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+
+/* Full-width input fields */
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+/* Set a style for all buttons */
+button {
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+/* Extra styles for the cancel button */
+.cancelbtn {
+  width: auto;
+  padding: 10px 18px;
+  background-color: #f44336;
+}
+
+/* Center the image and position the close button */
+.imgcontainer {
+  text-align: center;
+  margin: 24px 0 12px 0;
+  position: relative;
+}
+
+img.avatar {
+  width: 40%;
+  border-radius: 50%;
+}
+
+.container {
+  padding: 16px;
+}
+
+span.psw {
+  float: right;
+  padding-top: 16px;
+}
+
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+  padding-top: 60px;
+}
+
+/* Modal Content/Box */
+.modal-content {
+  background-color: #fefefe;
+  margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+  border: 1px solid #888;
+  width: 80%; /* Could be more or less, depending on screen size */
+}
+
+/* The Close Button (x) */
+.close {
+  position: absolute;
+  right: 25px;
+  top: 0;
+  color: #000;
+  font-size: 35px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: red;
+  cursor: pointer;
+}
+
+/* Add Zoom Animation */
+.animate {
+  -webkit-animation: animatezoom 0.6s;
+  animation: animatezoom 0.6s
+}
+
+@-webkit-keyframes animatezoom {
+  from {-webkit-transform: scale(0)} 
+  to {-webkit-transform: scale(1)}
+}
+  
+@keyframes animatezoom {
+  from {transform: scale(0)} 
+  to {transform: scale(1)}
+}
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 300px) {
+  span.psw {
+     display: block;
+     float: none;
+  }
+  .cancelbtn {
+     width: 100%;
+  }
+}
+</style>
 </head>
 <body>
 
-	<form action="https://firstprojectgg.herokuapp.com/">
-   <p><strong>Логин:</strong> 
-    <input maxlength="25" size="40" name="login"></p>
-   <p><strong>Пароль:</strong> 
-    <input type="password" maxlength="25" size="40" name="password"></p>
-   <p><input type="submit" value="Войти"></p>
+<button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
+
+<div id="id01" class="modal">
+  
+  <form class="modal-content animate" action="" method="post">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+      <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Avatar" class="avatar">
+    </div>
+
+    <div class="container">
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" id="fname"required>
+
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" id="lname"required>
+        
+      <button type="submit" onclick="myFunction()">Login</button>
+      <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+      </label>
+    </div>
+
+    <div class="container" style="background-color:#f1f1f1">
+      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+      <span class="psw">Forgot <a href="#">password?</a></span>
+      
+      <script lauguage="javascript">
+  function myFunction(){
+      var a = document.getElementById("fname").value
+      var b = document.getElementById("lname").value
+      var form = document.querySelector("form")
+
+       if (a=="aidana" && b == "12345"){
+        form.setAttribute('action', 'https://www.youtube.com/');
+       }else{
+        alert('permission denied')
+       }
+  }
+
+
+</script>
+
+
+
+    </div>
   </form>
+</div>
+<h3>Name: Aidana Akmatova</h3>
+
+<h3>Age: 19</h3>
+
+<h3>Status: Student</h3>
+<P>.</P>
+<P>.</P>
+<P>.</P>
+<P>.</P>
+<P>.</P>
+
+
+
+<h2>Me in social media</h2>
+
+
+<a href="#" class="fa fa-google"></a>
+
+<a href="https://youtube.com/channel/UCVhTYda6nxDhOIZ02TABHRg" class="fa fa-youtube"></a>
+<a href="https://instagram.com/aakmatova__?igshid=124mqrrhxdojp" class="fa fa-instagram"></a>
+
+<a href="#" class="fa fa-skype"></a>
+
+
+
+
+
 
 
 
